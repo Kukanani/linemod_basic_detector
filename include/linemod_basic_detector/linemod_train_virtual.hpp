a9f0c8edde26ec9e6ff5776fc38ffb1248d6ca16
Adam Allevato <adam.d.allevato@gmail.com>
2017-06-30 08:56:24 -0700

    Anoter bad OpenCV include

--- include/linemod_basic_detector/linemod_train_virtual.hpp
+++ include/linemod_basic_detector/linemod_train_virtual.hpp
@@ -57,7 +57,6 @@
 #else
   #include <opencv2/imgproc/imgproc.hpp>
   #include <opencv2/objdetect/objdetect.hpp>
-  #include <opencv2/objdetect.hpp>
   #include <opencv2/core/core.hpp>
   #include <opencv2/highgui/highgui.hpp>
 #endif
